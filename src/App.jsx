c8deefffe901204f42aaef8acb8083e3e6f20412
roro <bodouson28gmail.com>
2024-08-01 16:51:59 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Nav from './Nav.jsx'
+
+export default function App(){
+  return(
+    <div className="app">
+      <Nav />
+    </div>
+  )
+}
---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+
+
+export default function Nav(){
+    return(
+        <div className="nav-bar">
+            <h3>SonnaDev</h3>
+        </div>
+    )
+}
